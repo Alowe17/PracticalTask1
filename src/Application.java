8eb16841ec13438916b6396c8360d70a0222d7c3
alowe17 <p.danil51007@gmail.com>
2026-06-18 19:49:45 +0300

    Рефакторинг: Код ревью на основе комментарий
    - Реализован алгоритм Евклида для получения НОД
    - Получение НОК с использованием НОД
    - Логика парсинга вынесена в отдельный класс

--- src/Application.java
+++ src/Application.java
@@ -1,10 +1,12 @@
 import model.Numbers;
+import parser.NumberParser;
 import service.MathService;
 
 import java.util.Scanner;
 
 public class Application {
     private final MathService service = new MathService();
+    private final NumberParser parser = new NumberParser();
 
     public void start() {
         Scanner console = new Scanner(System.in);
@@ -12,12 +14,10 @@ public class Application {
         String number1 = console.nextLine();
         System.out.print("Введите число номер 2: ");
         String number2 = console.nextLine();
-        Numbers numbers = service.validate(number1, number2);
+        Numbers numbers = parser.parse(number1, number2);
 
-        int max = numbers.max();
-        int min = numbers.min();
-        int gcd = service.getGcd(max, min);
-        int lcm = service.getLcm(max, min);
+        int gcd = service.getGcd(numbers);
+        int lcm = service.getLcm(numbers);
 
         System.out.println();
         System.out.println("На вход поступили числа: " + number1 + " и " + number2);
--- src/service/MathService.java
+++ src/service/MathService.java
@@ -3,48 +3,20 @@ package service;
 import model.Numbers;
 
 public class MathService {
-    public Numbers validate(String string1, String string2) {
-        int number1;
-        int number2;
+    public int getGcd(Numbers numbers) {
+        int max = numbers.max();
+        int min = numbers.min();
 
-        try {
-            if (string1.isBlank())
-                throw new IllegalArgumentException("Необходимо ввести 1 число!");
-
-
-            number1 = Integer.parseInt(string1);
-        } catch (NumberFormatException exception) {
-            throw new NumberFormatException("Не удалось преобразовать 1 число!");
+        while (min != 0) {
+            int temp = min;
+            min = max % min;
+            max = temp;
         }
 
-        try {
-            if (string2.isBlank())
-                throw new IllegalArgumentException("Необходимо ввести 2 число!");
-
-            number2 = Integer.parseInt(string2);
-        } catch (NumberFormatException exception) {
-            throw new NumberFormatException("Не удалось преобразовать 2 число!");
-        }
-
-        if (number1 <= 0 || number2 <= 0)
-            throw new IllegalArgumentException("Числа должны быть строго положительными (больше 0)!");
-
-        return new Numbers(Math.max(number1, number2), Math.min(number1, number2));
+        return max;
     }
 
-    public int getGcd(int max, int min) {
-        for (int i = min; i >= 1; i--) {
-            if (max % i == 0 && min % i == 0)
-                return i;
-        }
-
-        throw new IllegalArgumentException("Не удалось определить НОД для чисел: " + max + " и " + min);
-    }
-
-    public int getLcm(int max, int min) {
-        for (int i = max; ; i += max) {
-            if (i % min == 0)
-                return i;
-        }
+    public int getLcm(Numbers numbers) {
+        return numbers.max() / getGcd(numbers) * numbers.min();
     }
 }
